a4914e090dec0a61b7e03d9f6f974835af6fe8a9
Andrés David Álvarez Tórrez <ndralvz@gmail.com>
2024-04-27 12:43:19 -0300

    crea al alumno juan pablo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
 const leer = require("./utils/leer");
+const crear = require("./utils/crear");
+
+crear({ nombre: "Juan Pablo", edad: 18, nota: 10, id: 2 });
 
 console.log(leer());
